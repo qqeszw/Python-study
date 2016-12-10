940cd0cf03578dd4f43ad2f3677395f2c3b87d02
qqeszw <qqeszw@163.com>
2016-12-10 14:42:14 +0800

    delete code test

--- absTest.py
+++ absTest.py
@@ -5,13 +5,3 @@ def my_abs(x):
         return x
     else:
         return -x
-
-# 请定义一个函数quadratic(a, b, c)，接收3个参数，返回一元二次方程：
-
-# ax2 + bx + c = 0
-
-def quadratic(a, b, c):
-    if not isinstance((a, b, c), (int, float)):
-        raise TypeError('bad operand type')
-
-    a * x * a * x + b * x + c = 0
